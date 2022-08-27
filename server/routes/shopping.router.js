20e77a54a6ad9c9805a59703cc59ee854a9d2165
Keith Cullinan <kfcullinan@gmail.com>
2022-08-27 12:30:07 -0500

    added in GET request

--- server/routes/shopping.router.js
+++ server/routes/shopping.router.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET request
-
+router.get('/', (req, res) => {
+    const sqlText = `SELECT * FROM list ORDER BY name, quantity DESC;`;
+    pool.query(sqlText)
+    .then((result) => {
+        console.log(`Received data from database`, result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log(`Error in making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+})
 
 // POST request
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+    const [shoppingList, setShoppingList] = useState([]);
+
+    const fetchList = () => {
+        axios({
+            method: 'GET',
+            url: '/list'
+        }).then(response => {
+            setShoppingList(response.data)
+        }).catch(error => {
+            console.log(error);
+            alert('Something is broken')
+        })
+    }
+
+
     return (
         <div className="App">
             <Header />
