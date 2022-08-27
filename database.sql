2f6913c93c49a8f55e1120f34827f39e593c1804
Aubree Herrion <aubreeherrion@gmail.com>
2022-08-27 11:13:32 -0500

    added data to database.sql file

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "list"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"quantity" FLOAT(8) 
+);
+-- GET
+SELECT * FROM "list";
+
+-- POST
+INSERT INTO "list" ("name", "quantity")
+VALUES ('bacon', 5.5), ('eggs', 2), ('shoes', 5);
