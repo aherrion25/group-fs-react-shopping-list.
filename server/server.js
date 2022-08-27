51c764e1aaef90a6aab628cc7a0194324b528add
Aubree Herrion <aubreeherrion@gmail.com>
2022-08-27 11:41:03 -0500

    created routers file for request

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5001;
+const listRouter = require('./routes/shopping.router.js');
+const PORT = process.env.PORT || 5007;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -10,7 +11,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
-
+app.use('/list', listRouter);
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
     console.log('Listening on port: ', PORT);
